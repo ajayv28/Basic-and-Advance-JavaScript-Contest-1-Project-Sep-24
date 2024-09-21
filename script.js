2887cb467f8a1ce898644cf9153eadd41318024d
ajayv28 <ajayvengateshbabu@gmail.com>
2024-09-21 18:28:45 +0300

    completed

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(employee => {
+    if(employee.profession === "developer")
+      console.log(employee);
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(employee =>{
+    if(employee.profession === "developer")
+      console.log(employee);
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  const employee = {id:4, name:"susan", age:"20", profession:"intern"};
+  arr.push(employee);
+  console.log(employee);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(employee => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArray = [
+    { id: 4, name: "ajay", age: "21", profession: "engineer" },
+    { id: 5, name: "ram", age: "22", profession: "doctor" },
+    { id: 6, name: "sam", age: "23", profession: "accountant" },
+  ];
+  
+  const combinedArray = arr.concat(newArray);
+  console.log(combinedArray);
 }
